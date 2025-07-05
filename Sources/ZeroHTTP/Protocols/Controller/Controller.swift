f2b1d4f7fed6b926333fde905ea87be914cd597f
Philipp3107 <philippkotte@icloud.com>
2025-07-06 00:19:40 +0200

    removed that

--- Sources/ZeroHTTP/Protocols/Controller/Controller.swift
+++ Sources/ZeroHTTP/Protocols/Controller/Controller.swift
@@ -15,17 +15,3 @@ public protocol Controller: NSObjectProtocol {
     @RouteBuilder var body: [Route] { get }
 }
 
-public extension Controller {
-    var body: [Route] {
-        // Verwende einen `Mirror`, um die Struktur der Controller-Instanz zu inspizieren.
-        let mirror = Mirror(reflecting: self)
-        
-        // Finde alle "Kinder" (Eigenschaften), die dem `AnyRoute`-Protokoll entsprechen,
-        // und extrahiere ihre `route`-Eigenschaft.
-        return mirror.children.compactMap { child in
-            // `child.value` ist der Wert der Eigenschaft (z.B. die PublicAccess-Instanz).
-            // Wir versuchen, ihn auf unser Marker-Protokoll zu casten.
-            return (child.value as? AnyRoute)?.route
-        }
-    }
-}

a3989738d291767d67afaee46ed06602c8c9ea6a
Philipp3107 <philippkotte@icloud.com>
2025-07-08 18:49:35 +0200

    protocol wrapper for simplicity

---
+++ Sources/ZeroHTTP/Protocols/Controller/AbstractController.swift
@@ -0,0 +1,11 @@
+//
+//  AbstractController.swift
+//  ZeroHTTP
+//
+//  Created by Philipp Kotte on 08.07.25.
+//
+
+
+public protocol AbstractController: DiscoverableController, Controller {
+    
+}
